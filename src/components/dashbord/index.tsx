90a6723611a755d63605057ed87a4823abe03b30
Behruz <ismoilovmashhurbek17@gmail.com>
2026-01-27 18:26:17 +0500

    chen

--- src/components/dashbord/index.tsx
+++ src/components/dashbord/index.tsx
@@ -3,9 +3,15 @@ import Products from "./products";
 
 const Dashboder = () => {
   return (
-    <div className="mt-5 grid grid-cols-[1fr_3fr] gap-5">
-      <Catigoriy />
-      <Products />
+    <div className="mt-5 flex flex-col md:grid md:grid-cols-[1fr_3fr] gap-5 px-4 md:px-0">
+      <aside className="w-full md:w-auto">
+        <div className="sticky top-0 z-10 bg-white py-2 md:static">
+          <Catigoriy />
+        </div>
+      </aside>
+      <main className="w-full">
+        <Products />
+      </main>
     </div>
   );
 };
