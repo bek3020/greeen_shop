b88216ff68e468dc490b6c529762d9dfc68ab025
Behruz <ismoilovmashhurbek17@gmail.com>
2026-01-29 16:33:03 +0500

    salom

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,23 @@
-import type { ProductTitleType } from "../@types";
+interface ProductTitleType {
+  id: number;
+  title: string;
+  route_path: string;
+}
 
-export const products_titles:ProductTitleType[] = [
-    {
-        id: 1,
-        title: "All Plants",
-        route_path:"all-plants "
-    },
-    {
-        id: 2,
-        title: "New Arrivals",
-        route_path:" new-arrivals"
-    },
-    {
-        id: 3,
-        title: "Sale",
-        route_path:"sale"
-    },
-]
+export const products_titles: ProductTitleType[] = [
+  {
+    id: 1,
+    title: "All Plants",
+    route_path: "all-plants",
+  },
+  {
+    id: 2,
+    title: "New Arrivals",
+    route_path: "new-arrivals",
+  },
+  {
+    id: 3,
+    title: "Sale",
+    route_path: "sale",
+  },
+];
